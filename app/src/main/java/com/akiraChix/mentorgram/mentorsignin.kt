4f4506c7ecae5c1b87a6ad6a93b1f0f9631acd80
gatchang-nyawargak <nyawargakgatchang@gmail.com>
2024-06-01 18:29:55 +0300

    signin files

--- app/src/main/java/com/akiraChix/mentorgram/mentorsignin.kt
+++ app/src/main/java/com/akiraChix/mentorgram/mentorsignin.kt
@@ -1,20 +1,23 @@
 package com.akiraChix.mentorgram
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class mentorsignin : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mentorsignin)
-        val signBtn = findViewById<Button>(R.id.signin)
-        signBtn.setOnClickListener {
+        val login = findViewById<TextView>(R.id.login)
+        login.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
