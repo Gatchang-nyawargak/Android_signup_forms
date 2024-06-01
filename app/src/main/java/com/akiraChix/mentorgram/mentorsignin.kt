7b8772791134b14e605d642137273512f1c441ac
gatchang-nyawargak <nyawargakgatchang@gmail.com>
2024-06-01 18:23:46 +0300

    signin files

--- app/src/main/java/com/akiraChix/mentorgram/mentorsignin.kt
+++ app/src/main/java/com/akiraChix/mentorgram/mentorsignin.kt
@@ -1,6 +1,8 @@
 package com.akiraChix.mentorgram
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,10 @@ class mentorsignin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mentorsignin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val signBtn = findViewById<Button>(R.id.signin)
+        signBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
